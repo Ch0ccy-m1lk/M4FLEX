92e5321aa665a2a4efe57b916e0f33121db9f1f7
Ch0ccy-m1lk <louellahanen@outlook.com>
2025-05-13 17:08:59 +0200

    IT WORKS

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+
+  static void Main(string[] args){
+
+    int episode = 3;
+    string episodeName = "THE BOLOGNA INCIDENT";
+    bool happyEnding = false;
+    float rating = 9.7f;
+
+    Console.WriteLine("episode: " + episode);
+    Console.WriteLine("episode name: " + episodeName);
+    Console.WriteLine("is there a happy ending? " + happyEnding);
+    Console.WriteLine("my personal rating of this episode? " + rating);
+
+
+
+
+  }
+
+}
